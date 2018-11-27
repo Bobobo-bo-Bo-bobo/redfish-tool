e799fb010a02bce28f88b3158b5a36b9cf50f771
Andreas Maus <maus@badphish.ypbind.de>
2018-11-27 10:17:27 +0100

    Merged JSON data types into general data type file data.go

--- src/redfish/data.go
+++ src/redfish/data.go
@@ -2,6 +2,86 @@ package redfish
 
 import "time"
 
+type OData struct {
+	Id           *string `json:"@odata.id"`
+	Type         *string `json:"@odata.type"`
+	Context      *string `json:"@odata.context"`
+	Members      []OData `json:"Members"`
+	MembersCount int     `json:"Members@odata.count"`
+}
+
+type baseEndpoint struct {
+	AccountService OData `json:"AccountService"`
+	Chassis        OData `json:"Chassis"`
+	Managers       OData `json:"Managers"`
+	SessionService OData `json:"SessionService"`
+	Systems        OData `json:"Systems"`
+}
+
+type sessionServiceEndpoint struct {
+	Enabled        *bool  `json:"ServiceEnabled"`
+	SessionTimeout int    `json:"SessionTimeout"`
+	Sessions       *OData `json:"Sessions"`
+}
+
+type Status struct {
+	State  string `json:"State"`
+	Health string `json:"Health"`
+}
+
+type SystemProcessorSummary struct {
+	Count  int    `json:"Count"`
+	Status Status `json:"Status"`
+}
+
+type SystemData struct {
+	UUID         *string `json:"UUID"`
+	Status       Status  `json:"Status"`
+	SerialNumber *string `json:"SerialNumber"`
+	//ProcessorSummary  *SystemProcessorSummary  `json:"ProcessorSummary"`
+	Processors *OData  `json:"Processors"`
+	PowerState *string `json:"Powerstate"`
+	Name       *string `json:"Name"`
+	Model      *string `json:"Model"`
+	//MemorySummary *SystemMemorySummary    `json:"MemorySummary"`
+	Memory             *OData  `json:"Memory"`
+	Manufacturer       *string `json:"Manufacturer"`
+	LogServices        *OData  `json:"LogServices"`
+	Id                 *string `json:"Id"`
+	EthernetInterfaces *OData  `json:"EthernetInterfaces"`
+	BIOSVersion        *string `json:"BiosVersion"`
+	BIOS               *OData  `json:"Bios"`
+	// Actions
+	SelfEndpoint *string
+}
+
+type AccountService struct {
+	AccountsEndpoint *OData `json:"Accounts"`
+	RolesEndpoint    *OData `json:"Roles"`
+}
+
+type AccountData struct {
+	Id       *string `json:"Id"`
+	Name     *string `json:"Name"`
+	UserName *string `json:"UserName"`
+	Password *string `json:"Password"`
+	RoleId   *string `json:"RoleId"`
+	Enabled  *bool   `json:"Enabled"`
+	Locked   *bool   `json:"Locked"`
+
+	SelfEndpoint *string
+}
+
+type RoleData struct {
+	Id                 *string  `json:"Id"`
+	Name               *string  `json:"Name"`
+	IsPredefined       *bool    `json:"IsPredefined"`
+	Description        *string  `json:"Description"`
+	AssignedPrivileges []string `json:"AssignedPrivileges"`
+	//    OemPrivileges   []string    `json:"OemPrivileges"`
+	SelfEndpoint *string
+}
+
 const (
 	REDFISH_GENERAL uint = iota
 	REDFISH_HP
