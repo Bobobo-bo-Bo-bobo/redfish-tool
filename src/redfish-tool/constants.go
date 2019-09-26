ea71cbd239946dfe08dddeb47790b7df74ff29f8
Andreas Maus <maus@ypbind.de>
2019-09-26 09:21:19 +0200

    Bump build release

--- src/redfish-tool/constants.go
+++ src/redfish-tool/constants.go
@@ -1,4 +1,4 @@
 package main
 
 // Note: Be consistent with "Semantic Versioning 2.0.0" - see https://semver.org/
-const version string = "1.2.0-2019.09.25"
+const version string = "1.2.0-2019.09.26"
