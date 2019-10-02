d3f66f0ef466319479fcc19c49dceacf63f833c8
Andreas Maus <maus@ypbind.de>
2019-10-02 07:27:27 +0200

    Bump build version

--- src/redfish-tool/constants.go
+++ src/redfish-tool/constants.go
@@ -1,7 +1,7 @@
 package main
 
 // Note: Be consistent with "Semantic Versioning 2.0.0" - see https://semver.org/
-const version string = "1.2.0-2019.10.01"
+const version string = "1.2.0-2019.10.02"
 const (
 	OUTPUT_TEXT uint = iota
 	OUTPUT_JSON
